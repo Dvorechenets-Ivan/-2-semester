3e4fd2a811525054976146dc92e460a4ccac5b15
Dvorechenets-Ivan <135117798+Dvorechenets-Ivan@users.noreply.github.com>
2024-02-13 12:32:28 +0200

    Add files via upload

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Timers;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        delegate void TimerDelegate();
+        
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer();
+            int intervalInSeconds = 5;
+
+            timer.Interval = intervalInSeconds * 1000;
+            TimerDelegate timerDelegate = new TimerDelegate(MethodToExecute);
+            timerDelegate += AnotherMethodToExecute;
+            timer.Elapsed += (sender, e) =>timerDelegate.Invoke();
+            
+            timer.Start();
+           
+            Console.WriteLine();
+
+            Console.ReadKey();
+            timer.Stop();
+        }
+        static void MethodToExecute()
+        {
+            Console.WriteLine(""+DateTime.Now);
+        }
+        static void AnotherMethodToExecute()
+        {
+            Console.WriteLine("AnotherMethodToExecute" + DateTime.Now);
+        }
+
+
+    }
+
+}
